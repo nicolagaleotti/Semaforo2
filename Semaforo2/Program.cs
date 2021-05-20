47d7b5e47185d0f9e4e185911b0b41b180b3fbaa
Nicola Galeotti <niccio2802@outlook.it>
2021-05-20 10:55:15 +0200

    Add project files.

---
+++ Semaforo2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Semaforo2
+{
+    class Program
+    {
+        static int s = 0;
+        public static SemaphoreSlim t = new SemaphoreSlim(0);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread t1 = new Thread(() => Procedura1());
+                Thread t2 = new Thread(() => Procedura2());
+                t1.Start();
+                t2.Start();
+                while (t1.IsAlive) { }
+                while (t2.IsAlive) { }
+                Console.WriteLine(s);
+            }
+
+        }
+        public static void Procedura1()
+        {
+            Console.WriteLine("Inserisci a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci b:");
+            int b = int.Parse(Console.ReadLine());
+            s = a + b;
+            Console.WriteLine($"{a}, {b}");
+            t.Release();
+        }
+
+        public static void Procedura2()
+        {
+            Random n = new Random();
+            int c = n.Next(10, 99);
+            t.Wait();
+            Console.WriteLine($"Random : {c}");
+            s = s + c;
+            Console.WriteLine($"Somma : {s}");
+        }
+    }
+}
